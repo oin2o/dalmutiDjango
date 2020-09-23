93e35b906f5a363ebb4398ddbe4cca286e0ae166
oin2o <oin2o.h@gmail.com>
2020-09-23 09:01:31 +0900

    board dalmuti setting allowed hosts

--- plateau/plateau/settings.py
+++ plateau/plateau/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w=1f1qg0tqpn+&=yq5)a8^bp37*=4zt%uf&a%4r@cgwt6q&xva'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
