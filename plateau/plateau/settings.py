96f6d8620650f62a56b0487ed9e8244691a97777
oin2o <oin2o.h@gmail.com>
2020-09-24 21:44:56 +0900

    board dalmuti modify static file dir

--- plateau/plateau/settings.py
+++ plateau/plateau/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'dalmuti', 'static')
+    os.path.join(BASE_DIR, 'dalmuti', 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
