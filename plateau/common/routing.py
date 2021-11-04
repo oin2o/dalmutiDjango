57a9378ef631038a13574c333ad59be5f5572203
oin2o <oin2o.h@gmail.com>
2021-11-04 21:16:49 +0900

    add as_asgi in routing

--- plateau/common/routing.py
+++ plateau/common/routing.py
@@ -4,10 +4,10 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    url(r'dalmuti/(?P<gamename>\w+)/(?P<username>\w+)', consumers.common_consumer),
-    url(r'dalmutip/(?P<gamename>\w+)/(?P<username>\w+)', consumers.common_consumer),
-    url(r'dalmutid/(?P<gamename>\w+)/(?P<username>\w+)', consumers.common_consumer),
-    url(r'liar/(?P<gamename>\w+)/(?P<username>\w+)', consumers.common_consumer),
-    url(r'davinci/(?P<gamename>\w+)/(?P<username>\w+)', consumers.common_consumer),
-    url(r'dixit/(?P<gamename>\w+)/(?P<username>\w+)', consumers.common_consumer),
+    url(r'dalmuti/(?P<gamename>\w+)/(?P<username>\w+)', consumers.common_consumer.as_asgi()),
+    url(r'dalmutip/(?P<gamename>\w+)/(?P<username>\w+)', consumers.common_consumer.as_asgi()),
+    url(r'dalmutid/(?P<gamename>\w+)/(?P<username>\w+)', consumers.common_consumer.as_asgi()),
+    url(r'liar/(?P<gamename>\w+)/(?P<username>\w+)', consumers.common_consumer.as_asgi()),
+    url(r'davinci/(?P<gamename>\w+)/(?P<username>\w+)', consumers.common_consumer.as_asgi()),
+    url(r'dixit/(?P<gamename>\w+)/(?P<username>\w+)', consumers.common_consumer.as_asgi()),
 ]
